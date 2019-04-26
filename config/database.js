3728c15606a6841e0c8ffcd1a115b5f445774a84
Abhinav Chaudhary <31585519+abhinav00041@users.noreply.github.com>
2019-04-27 02:07:25 +0530

    Update database.js
    Read DB params from env

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ const Sequelize = require("sequelize");
 
 // Option 1: Passing parameters separately
 const db = new Sequelize(
-  "d7fb19pgs8e316",
-  "zwdfmfezofalwg",
-  "ed6aea6560cf582a8566b53c7c29dba8dd783d76eb7fb07e960e843de62107c3",
+ process.env.DB,
+ process.env.USER,
+ process.env.PASSWORD,
   {
-    host: "ec2-75-101-131-79.compute-1.amazonaws.com",
+    host: process.env.DB_URI,
     dialect: "postgres",
     pool: {
       max: 5,
@@ -21,7 +21,7 @@ const db = new Sequelize(
 
 // Option 2: Using a connection URI
 // const db = new Sequelize(
-//   "postgres://zwdfmfezofalwg:ed6aea6560cf582a8566b53c7c29dba8dd783d76eb7fb07e960e843de62107c3@ec2-75-101-131-79.compute-1.amazonaws.com:5432/d7fb19pgs8e316"
+//   "db_uri"
 // );
 
 module.exports = db;
