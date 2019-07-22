dc03700597cc30767ecc0d18a48f72931923f1f1
Abhinav Chaudhary <31585519+abhinav00041@users.noreply.github.com>
2019-07-22 13:23:15 +0530

    Update server.js
    Update CORS issue

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ db.authenticate()
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "lillycss.herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 // app.post("/login", (req, res) => {
 //   const user = req.body.username;
 //   const password = req.body.password;
