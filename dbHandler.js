f0239015568687ffa102b3cacdec3d331b29fe1d
Kuldeep Tyagi - Network <tyagi_kuldeep@network.lilly.com>
2019-07-29 16:55:10 +0530

    submit and save css work commit

--- dbHandler.js
+++ dbHandler.js
@@ -3,6 +3,9 @@ const projectdetails = require("./models/ProjectDetails");
 const cssreview = require("./models/CssReview");
 const projectStatus = require("./models/ProjectStatus");
 const sequelize = require("sequelize");
+const async = require('async');
+const nodemailer = require('nodemailer');
+
 
 const getUsers = (req, res) => {
   customerdetails.findAll().then(users => {
@@ -47,113 +50,227 @@ getUserById = async (req, res) => {
 /** Create/Save new Css */
 const createCss = (req, res) => {
   console.log(req.body);
-
-  const { projectid, userid, feedback } = req.body;
-  cssreview
-    .findAll({
-      where: {
-        projectId: projectid,
-        userId: userid,
-        status: "saved"
-      }
-    })
-    .then(data => {
-      if (!data.length) {
-        // Save new entry
-        cssreview
-          .upsert({
-            projectId: projectid,
-            userId: userid,
-            feedBack: feedback,
-            status: "saved"
-          })
-          .then(result => {
-            console.log("Result" + JSON.stringify(result));
-            updateStaus("saved", projectid, userid);
-            // response.sendStatus(201).json(result);
-            if (result) {
-              res.status(200);
-              res.send("Successfully stored");
-            } else {
-              res.status(200);
-              res.send("Successfully inserted");
-            }
-          })
-          .catch(err => {
-            console.log("Error in Add Project", err);
-          });
-      } else {
-        cssreview
-          .update(
-            { feedBack: feedback },
-            {
-              where: {
-                projectId: projectid,
-                userId: userid
+  async.map(req.body, function(object, cb) {
+    const { projectid, userid, feedback } = object;
+    cssreview
+      .findAll({
+        where: {
+          projectId: projectid,
+          userId: userid,
+          status: "saved"
+        }
+      })
+      .then(data => {
+        if (!data.length) {
+          // Save new entry
+          cssreview
+            .upsert({
+              projectId: projectid,
+              userId: userid,
+              feedBack: feedback,
+              status: "saved"
+            })
+            .then(result => {
+              console.log("Result" + JSON.stringify(result));
+              updateStaus("saved", projectid, userid);
+              // response.sendStatus(201).json(result);
+              if (result) {
+                cb(null,{status:"ok", statuscode:200,value :"Successfully stored"});
+              } else {
+                cb(null,{status : "ok",statuscode:200 ,value :"Successfully inserted"});
               }
-            }
-          )
-          .then(result11 => {
-            res.status(200).send("Updated saved css");
-          });
-      }
-    });
+            })
+            .catch(err => {
+              console.log("Error in Add Project", err);
+              cb({status : "error",statuscode:500 ,value :"Error in Add Project"+ err});
+  
+            });
+        } else {
+          cssreview
+            .update(
+              { feedBack: feedback },
+              {
+                where: {
+                  projectId: projectid,
+                  userId: userid
+                }
+              }
+            )
+            .then(result11 => {
+              cb(null,{status : "ok",statuscode:200 ,value :"Updated saved css"});
+            });
+        }
+      });  
+  }, function(error, results) {
+    if (error) return res.status(500).send(error);
+    console.log(results);
+    res.send(results);
+  });
+  // const { projectid, userid, feedback } = req.body;
+  // cssreview
+  //   .findAll({
+  //     where: {
+  //       projectId: projectid,
+  //       userId: userid,
+  //       status: "saved"
+  //     }
+  //   })
+  //   .then(data => {
+  //     if (!data.length) {
+  //       // Save new entry
+  //       cssreview
+  //         .upsert({
+  //           projectId: projectid,
+  //           userId: userid,
+  //           feedBack: feedback,
+  //           status: "saved"
+  //         })
+  //         .then(result => {
+  //           console.log("Result" + JSON.stringify(result));
+  //           updateStaus("saved", projectid, userid);
+  //           // response.sendStatus(201).json(result);
+  //           if (result) {
+  //             res.send({status:"ok", statuscode:200,value :"Successfully stored"});
+  //           } else {
+  //             res.send({status : "ok",statuscode:200 ,value :"Successfully inserted"});
+  //           }
+  //         })
+  //         .catch(err => {
+  //           console.log("Error in Add Project", err);
+  //           res.status(500).send({status : "error",statuscode:500 ,value :"Error in Add Project"+ err});
+
+  //         });
+  //     } else {
+  //       cssreview
+  //         .update(
+  //           { feedBack: feedback },
+  //           {
+  //             where: {
+  //               projectId: projectid,
+  //               userId: userid
+  //             }
+  //           }
+  //         )
+  //         .then(result11 => {
+  //           res.status(200).send({status : "ok",statuscode:200 ,value :"Updated saved css"});
+  //         });
+  //     }
+  //   });
+
+
 };
 
 /** Submit new Css */
 const submitCss = (req, res) => {
   console.log(req.body);
-  const { projectid, userid, feedback } = req.body;
-  cssreview
-    .findAll({
-      where: {
-        projectId: projectid,
-        userId: userid,
-        status: "saved"
-      }
-    })
-    .then(data => {
-      if (!data.length) {
-        // Save new entry
-        cssreview
-          .upsert({
-            projectId: projectid,
-            userId: userid,
-            feedBack: feedback,
-            status: "submited"
-          })
-          .then(result => {
-            console.log("Result" + JSON.stringify(result));
-            updateStaus("submited", projectid, userid);
-            // response.sendStatus(201).json(result);
-            if (result) {
-              res.status(200);
-              res.send("Successfully stored");
-            } else {
-              res.status(200);
-              res.send("Successfully inserted");
-            }
-          })
-          .catch(err => {
-            console.log("Error in Add Project", err);
-          });
-      } else {
-        cssreview
-          .update(
-            { feedBack: feedback, status: "submited" },
-            {
-              where: {
-                projectId: projectid,
-                userId: userid
+  async.map(req.body, function(object, cb) {
+    (async function () {
+      await sendEmail().catch((err)=>{console.log(err)});
+    }())
+    const { projectid, userid, feedback } = object;
+    cssreview
+      .findAll({
+        where: {
+          projectId: projectid,
+          userId: userid,
+          status: "saved"
+        }
+      })
+      .then(data => {
+        if (!data.length) {
+          // Save new entry
+          cssreview
+            .upsert({
+              projectId: projectid,
+              userId: userid,
+              feedBack: feedback,
+              status: "submited"
+            })
+            .then(result => {
+              console.log("Result" + JSON.stringify(result));
+              updateStaus("submited", projectid, userid);
+              // response.sendStatus(201).json(result);
+              if (result) {
+                cb(null,{status:"ok", statuscode:200,value :"Successfully stored"});
+              } else {
+                cb(null,{status:"ok", statuscode:200,value :"Successfully inserted"});
               }
-            }
-          )
-          .then(result11 => {
-            updateStaus("submited", projectid, userid);
-            res.status(200).send("Submit css");
-          });
-      }
-    });
+            })
+            .catch(err => {
+              cb({status : "error",statuscode:500 ,value :"Error in Add Project"+ err});
+            });
+        } else {
+          cssreview
+            .update(
+              { feedBack: feedback, status: "submited" },
+              {
+                where: {
+                  projectId: projectid,
+                  userId: userid
+                }
+              }
+            )
+            .then(result11 => {
+              updateStaus("submited", projectid, userid);
+            cb(null,{status : "ok",statuscode:200 ,value :"Submit css"});
+            });
+        }
+      });
+  }, function(error, results) {
+    if (error) return res.status(500).send(error);
+    console.log(results);
+    res.send(results);
+  });
+
+  // const { projectid, userid, feedback } = req.body;
+  // cssreview
+  //   .findAll({
+  //     where: {
+  //       projectId: projectid,
+  //       userId: userid,
+  //       status: "saved"
+  //     }
+  //   })
+  //   .then(data => {
+  //     if (!data.length) {
+  //       // Save new entry
+  //       cssreview
+  //         .upsert({
+  //           projectId: projectid,
+  //           userId: userid,
+  //           feedBack: feedback,
+  //           status: "submited"
+  //         })
+  //         .then(result => {
+  //           console.log("Result" + JSON.stringify(result));
+  //           updateStaus("submited", projectid, userid);
+  //           // response.sendStatus(201).json(result);
+  //           if (result) {
+  //             res.send({status:"ok", statuscode:200,value :"Successfully stored"});
+  //           } else {
+  //             res.send({status:"ok", statuscode:200,value :"Successfully inserted"});
+  //           }
+  //         })
+  //         .catch(err => {
+  //           res.status(500).send({status : "error",statuscode:500 ,value :"Error in Add Project"+ err});
+  //         });
+  //     } else {
+  //       cssreview
+  //         .update(
+  //           { feedBack: feedback, status: "submited" },
+  //           {
+  //             where: {
+  //               projectId: projectid,
+  //               userId: userid
+  //             }
+  //           }
+  //         )
+  //         .then(result11 => {
+  //           updateStaus("submited", projectid, userid);
+  //           res.status(200).send({status : "ok",statuscode:200 ,value :"Submit css"});
+  //         });
+  //     }
+  //   });
 };
 
 /** Get css by Id */
@@ -190,7 +307,32 @@ async function updateStaus(status, project, user) {
       console.log(update);
     });
 }
-
+const sendEmail = async (object)=>{
+  return new Promise((resolve,reject)=>{
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: 587,
+    secure: false,
+    auth: {
+           user: 'csstcs2@gmail.com',
+           pass: 'qwerty@123'
+       },
+  
+   });
+   const mailOptions = {
+    from: 'csstcs2@gmail.com', // sender address
+    to: 'ktyagi788@gmail.com', // list of receivers
+    subject: 'Subject of your email', // Subject line
+    html: '<p>Your html here</p>'// plain text body
+  };
+  transporter.sendMail(mailOptions, function (err, info) {
+    if(err)
+      reject(err)
+    else
+      resolve(info);
+ });
+})
+}
 module.exports = {
   getUsers,
   getUserById,
